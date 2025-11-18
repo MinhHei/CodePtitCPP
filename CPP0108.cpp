e50af16f3e22708a9e1f1a1503f997ccd5d7cb73
MinhHei <haisat1712@gmail.com>
2025-11-18 09:52:04 +0700

    Add files via upload

---
+++ CPP0108.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a[10] = {5,20,38,50,38,20,8,5,0};
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int z; cin >> z;
+        cout << a[z-1] << endl;
+    }
+}
---
+++ CPP0116.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int nt(int n){
+    if(n==1) return 1;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0) return i;
+    }
+    return n;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        for(int i=1;i<=n;i++){
+            cout << nt(i) << ' ';
+        }
+        cout << endl;
+    }
+}
